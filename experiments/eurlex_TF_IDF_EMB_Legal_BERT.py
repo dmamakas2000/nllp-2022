fe592bcf31283bfb917f81b6fe140a7135c869c7
Dimitrios Mamakas <80553431+dmamakas2000@users.noreply.github.com>
2022-09-30 23:16:09 +0300

    Fixed minor bug on eurlex script

--- experiments/eurlex_TF_IDF_EMB_Legal_BERT.py
+++ experiments/eurlex_TF_IDF_EMB_Legal_BERT.py
@@ -231,7 +231,10 @@ def main():
         predict_dataset = load_dataset("lex_glue", "eurlex", split="test", cache_dir=model_args.cache_dir)
 
     # Labels
-    label_list = list(range(100))
+    label_list = [119, 120, 114, 90, 28, 29, 30, 82, 87, 8, 44, 31, 33, 94, 22, 14, 52, 91, 92, 13, 89, 86, 118, 93, 12, 68, 83,
+                    98, 11, 7, 32, 115, 96, 79, 116, 106, 81, 75, 117, 112, 59, 6, 77, 95, 72, 108, 60, 99, 74, 24, 27, 34, 58,
+                    66, 84, 61, 16, 107, 20, 43, 97, 105, 76, 67, 80, 57, 63, 37, 36, 85, 5, 109, 69, 38, 78, 39, 49, 23, 42, 100,
+                    17, 70, 9, 51, 113, 103, 102, 110, 0, 41, 111, 101, 35, 64, 10, 121, 21, 26, 71, 122]
     num_labels = len(label_list)
 
     # Load pretrained model and tokenizer
